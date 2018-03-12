e42d8ab22a77370417f190e658dad23a6449307f
Neilton Galvao <neilton@tjro.jus.br>
2018-03-12 17:32:02 -0400

    commit inicial

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>{{ titulo }}</h1>
+    <ul>
+      <li v-for="foto of fotos">
+        <img :src="foto.url" :alt="foto.titulo">
+      </li>
+          
+    </ul>  
+  </div>  
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    titulo: 'AluraPic',
+      fotos :[
+        {
+          url : 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTwV4kVzT5McBdGSgqlVeRzubrNH_mOrrkKseDOGFURq20HmsrelEfMU7It',
+          titulo: 'Cachorro'
+        },
+        {
+          url: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTwV4kVzT5McBdGSgqlVeRzubrNH_mOrrkKseDOGFURq20HmsrelEfMU7It',
+          titulo: 'Cachorro2'
+        }
+      ]          
+    }  
+  }
+}    
+</script>
+
+<style>
